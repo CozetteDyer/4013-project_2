b2b5c38131b16ca46dd46de57d0ea5a49fefe78e
CozetteDyer <51537233+CozetteDyer@users.noreply.github.com>
2022-12-02 15:17:20 -0600

    updated with SD card

--- main_imu-blue/main_imu-blue.ino
+++ main_imu-blue/main_imu-blue.ino
@@ -2,7 +2,11 @@
 #include <Wire.h> // for I2C for IMU
 #include <Adafruit_LSM6DS33.h> // 6-DoF Accelerometer and Gyroscope Sensor
 #include <Adafruit_LIS3MDL.h> // magnetometer
+#include <SD.h> // SD card lib
+#include <SPI.h> // for SD card
 
+File sdFile;
+const int chipSelect = BUILTIN_SDCARD;
 Adafruit_LSM6DS33 accelerometer; // accel and gyro 
 Adafruit_LIS3MDL magnetometer; // magnetometer
 
@@ -14,59 +18,118 @@ SoftwareSerial bluetooth(rx, tx);
 //SoftwareSerial gps(dummy1, dummy2);
 //SoftwareSerial imu(dummy3, dummy4);
 
-// void IMU ()
-// {
-//   // By default, the I2C address is 0x6A.  
-//   // If you add a jumper from DO to 3.3V, the address will change to 0x6B
+void IMU () {
+  // By default, the I2C address is 0x6A.  
+  // If you add a jumper from DO to 3.3V, the address will change to 0x6B
 
-//     // finish
-  
-// }
+  // *********************************************************************     IMU 
+    sensors_event_t accel, gyro, mag, temp;
+    accelerometer.getEvent(&accel, &gyro, &temp);
+    magnetometer.getEvent(&mag);
+    String ax, ay, az;
+    String gx, gy, gz;
+    String mx, my, mz;
+    String tempV;
+    String accelString, gyroString, magString;
 
-// void GPS()
-// {
-//     // finish
-// }
+    /* 
+        I2C is 8-bit data bus, where address can be 7 bit or 10 bit, 
+        you can't send more than 8 bit at a time 
+        // String( val, decimal places)
+    */
+        
+    /* Display the results (acceleration is measured in m/s^2) */
+    ax = String(accel.acceleration.x, 4); // Accel X
+    ay = String(accel.acceleration.y, 4); // Accel Y
+    az = String(accel.acceleration.z, 4); // Accel Z
+    accelString = "AX = " + ax + "\tAY = " + ay + "\tAZ = " + az; 
+    Serial.println(accelString);
+
+    /* Display the results (rotation is measured in rad/s) */
+    gx = String(gyro.gyro.x, 4);
+    gy = String(gyro.gyro.y, 4);
+    gz = String(gyro.gyro.z, 4);
+    gyroString = "GX = " + gx + "\tGY = " + gy + "\tGZ = " + gz; 
+    Serial.println(gyroString);
+
+    /* Display the results (magnetic field is measured in uTesla) */
+    mx = String(mag.magnetic.x, 4);
+    my = String(mag.magnetic.y, 4);
+    mz = String(mag.magnetic.z, 4);
+    magString = "MX = " + mx + "\tMY = " + my + "\tMZ = " + mz; 
+    Serial.println(magString);
+
+    /* Display the results (magnetic field is measured in uTesla) */
+    tempV = String(temp.temperature, 2); // 2 decimal places
+    Serial.println("temp " + tempV);
+    delay(1000);
+    // *********************************************************************     end of IMU
+}   // end of imu()
+
+void GPS() {
+    // finish
+}
+
+void writeSD() {
+    sdFile = SD.open("test.txt", FILE_WRITE); // change file name ***
+
+    // if the file opened okay, write to it:
+    if (sdFile) {
+        Serial.print("Writing to test.txt...");
+        sdFile.println("test 1, 2, 3.");
+        // close the file:
+        sdFile.close();
+        Serial.println("writing complete.");
+    } 
+    
+    else { // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+} // end of write to SD card function
+
+void readSD() {   
+    sdFile = SD.open("test.txt"); // re-open the file for reading
+    if (sdFile) {
+        Serial.println("test.txt:");
+        // read from the file until there's nothing else in it:
+        while (sdFile.available()) {
+            Serial.write(sdFile.read());
+        }
+        
+        sdFile.close(); // close the file
+        } 
+    
+    else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+
+} // read from SD card function 
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
+    Serial.println("CODE: main_sd-imu!");
 
-  //gps.begin(9600);
-    Serial.begin(11520);
-  //imu.begin(115200);
- // bluetooth.begin(9600);
+    //gps.begin(9600);
+    Serial.begin(11520); // imu.begin(115200);
+  
+    // bluetooth.begin(9600);
+    //   delay(100);
+    //   bluetooth.println("Let's Start!");
+    //   delay(100);
 
-//   delay(100);
-//   bluetooth.println("Let's Start!");
+    // *********************************************************************     IMU-setup
+    bool accelerometer_success; //, magnetometer_success;
 
-//   delay(100);
-
-  //                                                                                IMU Set-up
-
-    Serial.println("CODE: main_imu-blue!");
-    bool accelerometer_success, magnetometer_success;
-
-  // hardware I2C mode, can pass in address & alt Wire
-
+    // hardware I2C mode, can pass in address & alt Wire
     accelerometer_success = accelerometer.begin_I2C();
-    magnetometer_success = magnetometer.begin_I2C();
+    // magnetometer_success = magnetometer.begin_I2C();
 
     if (!accelerometer_success){
     Serial.println("Failed to find accelerometer chip");
     }
 
-    /* FOR some reson this doesnt work. Teensy cant seem to find the chip, but we still get data. Not sure if data is correct 
-     *  Need to revisit!!
-     */
-//    if (!magnetometer_success){
-//    Serial.println("Failed to find magnetometer chip");
-//    }
-//    if (!(accelerometer_success && magnetometer_success)) {
-//    while (1) {
-//        delay(1); // 10
-//        }
-//    }
-    Serial.println("LSM6DS and LIS3MDL Found!");
+    Serial.println("IMU found!");
     // Double check these values!!                                                    *FINISH*
     accelerometer.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
     accelerometer.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
@@ -83,6 +146,28 @@ void setup() {
                                 true, // polarity
                                 false, // don't latch
                                 true); // enabled!
+    // *********************************************************************     end of IMU - setup
+    
+    
+    // *********************************************************************     SD card setup
+    SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+    SPI.setSCK(14);  // Audio shield has SCK on pin 14
+
+    // Open serial communications and wait for port to open:
+    Serial.begin(9600);
+    while (!Serial) {
+        ; // wait for serial port to connect.
+    }
+
+    Serial.print("Initializing SD card...");
+
+    if (!SD.begin(chipSelect)) {
+        Serial.println("initialization failed");
+        return;
+    }
+    Serial.println("initialization done.");
+
+// *********************************************************************     SD card setup
 
 } // end of void setup()
 
@@ -90,53 +175,17 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-//   if (bluetooth.available())
-//   {
-//     char c = (char)bluetooth.read();
-//     Serial.write(c);
-
-//   }
-
-
-    // IMU 
-    sensors_event_t accel, gyro, mag, temp;
-    accelerometer.getEvent(&accel, &gyro, &temp);
-    magnetometer.getEvent(&mag);
-    float ax, ay, az;
-    float gx, gy, gz;
-    float mx, my, mz;
-    float tempV;
-    String accelString, gyroString, magString;
+    //   if (bluetooth.available())
+    //   {
+    //     char c = (char)bluetooth.read();
+    //     Serial.write(c);
+    //   }
 
 
-    /* I2C is 8-bit data bus, where address can be 7 bit or 10 bit, 
-       you can't send more than 8 bit at a time */
+    IMU(); // get data from IMU
+    writeSD();
+    readSD();
 
-    /* Display the results (acceleration is measured in m/s^2) */
-    ax = accel.acceleration.x; // Accel X
-    ay = accel.acceleration.y; // Accel Y
-    az = accel.acceleration.z; // Accel Z
-    accelString = "AX = " + ax + "\tAY = " + ay + "\tAZ = " + az; 
-    Serial.println(accelString);
-
-    /* Display the results (rotation is measured in rad/s) */
-    gx = gyro.gyro.x;
-    gy = gyro.gyro.y;
-    gz = gyro.gyro.z;
-    gyroString = "GX = " + gx + "\tGY = " + gy + "\tGZ = " + gz; 
-    Serial.println(gyroString);
-
-    /* Display the results (magnetic field is measured in uTesla) */
-    mx = mag.magnetic.x;
-    my = mag.magnetic.y;
-    mz = mag.magnetic.z;
-    magString = "MX = " + mx + "\tMY = " + my + "\tMZ = " + mz; 
-    Serial.println(magString);
-
-    /* Display the results (magnetic field is measured in uTesla) */
-    tempV = temp.temperature;
-    Serial.println("temp " + tempV);
-    delay(1000);
 
  
 } // end of void loop()
